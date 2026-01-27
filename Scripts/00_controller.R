80f1ee3b287a5beee9f9cde3cd4f05c9d503b6a0
Matt Sasaki <mcsasaki20@gmail.com>
2026-01-27 12:58:01 -0500

    Updating analysis after adding species-level IDs

--- Scripts/00_controller.R
+++ Scripts/00_controller.R
@@ -12,31 +12,34 @@ knit_manuscript = F #Compiles manuscript draft
 ############################
 
 if(process_data == T){
+  initial_data = read.csv(file = "Raw_data/ctmax_data.csv") %>% 
+    drop_na(ctmax) %>% 
+    select(-order, -genus, "exp_notes" = notes) 
+  
+  ids = read.csv(file = "Raw_data/specimen_ids.csv", dec = ",", sep = ";") %>%  
+    janitor::clean_names() %>% select(-x, -x_1) %>% 
+    separate_wider_delim(code_order, delim = ",", names = c("specimen", "order")) %>%
+    separate_wider_position(specimen, widths = c(data_sheet = 2, tube = 2)) %>%
+    mutate(order = str_remove_all(order, "\""), 
+           species = str_trim(species), 
+           species = if_else(species == "", "sp.", species),
+           author = if_else(author == "", NA, str_trim(author)),
+           eggs = if_else(eggs == "", "absent", "present"),
+           data_sheet = as.numeric(data_sheet),
+           tube = as.numeric(tube)) %>%  
+    select(data_sheet:body_length_um, "id_notes" = notes) %>% 
+    mutate(data_sheet = if_else(data_sheet == 30, 34, data_sheet))
+  
+  ctmax_data = left_join(initial_data, ids)
+  
+  write.csv(ctmax_data, "Output/Output_data/ctmax_data.csv", row.names = F)
+  
   source(file = "Scripts/01_data_processing.R")
 }
 
 ##################################
 ### Read in the PROCESSED data ###
 ##################################
-
-ctmax_data = read.csv(file = "Raw_data/ctmax_data.csv") %>% 
-  drop_na(ctmax)
-
-read.csv(file = "Raw_data/ctmax_data.csv") %>%  
-  filter(order != "<NA>") %>%  
-  mutate("sheet_char" = if_else(data_sheet <10,
-                                as.character(paste0(0, data_sheet)),
-                                as.character(data_sheet)),
-         "tube_char" = if_else(tube <10,
-                                as.character(paste0(0, tube)),
-                                as.character(tube)), 
-         "code" = paste0(sheet_char, tube_char)) %>% 
-  select(code, order, site) %>% 
-  filter(order == "Cyclopoid") %>% 
-  mutate("species" = "") %>% 
-  select(-order) %>% 
-  write.csv(file = "Output/Output_data/cyclopoid_ids.csv", row.names = F)
-  
 
 if(make_report == T){
   render(input = "Output/Reports/report.Rmd", #Input the path to your .Rmd file here
